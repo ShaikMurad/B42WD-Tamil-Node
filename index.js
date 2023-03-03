a57b2c4d30e4a38a46e142d8ff6c809faf906ea1
ShaikMurad <shaikmurad786@gmail.com>
2023-03-03 17:10:03 +0530

    Organized Router

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ dotenv.config();
 // const express = require("express"); // "type": "commonjs"
 import express from "express"; // "type": "module"
 import { Db, MongoClient } from "mongodb";
+import moviesRouter from "./router/movies.js";
+
 const app = express();
 
 console.log(process.env.MONGO_URL);
@@ -15,7 +17,7 @@ const PORT = process.env.PORT; // Auto-assignable
 
 const MONGO_URL = process.env.MONGO_URL;
 
-const client = new MongoClient(MONGO_URL); // dial
+export const client = new MongoClient(MONGO_URL); // dial
 // Top level await
 await client.connect(); // call
 console.log("Mongo is connected !!!  ");
@@ -29,79 +31,8 @@ app.get("/", function (request, response) {
 });
 
 // http://loca/lhost:4000/movies
-app.get("/movies", async function (request, response) {
-  // db.movies.find({})
 
-  // Cursor -> Pagination (20) | Cursor -> Array (toArray())
-  const movies = await client
-    .db("B42WD")
-    .collection("movies")
-    .find({})
-    .toArray();
-  console.log(movies);
-  response.send(movies);
-});
-
-// Dummy -> Mock api
-app.get("/movies/:id", async function (request, response) {
-  const { id } = request.params;
-  console.log(id);
-  // db.movies.findOne({id:"1000"})
-
-  // const movie = movies.find((mv)=> mv.id === id);
-  const movie = await client
-    .db("B42WD")
-    .collection("movies")
-    .findOne({ id: id });
-
-  console.log(movie);
-
-  movie
-    ? response.send(movie)
-    : response.status(404).send({ message: "Movie not found" });
-});
-
-app.post("/movies", async function (request, response) {
-  const data = request.body;
-  console.log(data);
-
-  // db.movies.insertMany();
-  const result = await client.db("B42WD").collection("movies").insertMany(data);
-  response.send(result);
-});
-
-app.delete("/movies/:id", async function (request, response) {
-  const { id } = request.params;
-  console.log(id);
-  // db.movies.deleteOne({id:"1000"})
-
-  const result = await client
-    .db("B42WD")
-    .collection("movies")
-    .deleteOne({ id: id });
-
-  console.log(result);
-
-  result.deletedCount >= 1
-    ? response.send({ message: "Movie deleted successfully" })
-    : response.status(404).send({ message: "Movie not found" });
-});
-
-// Update
-app.put("/movies/:id", async function (request, response) {
-  const { id } = request.params;
-  const data = request.body;
-  console.log(data);
-  console.log(id);
-  // db.movies.updateOne({id:id}, {$set:data})
-
-  // const movie = movies.find((mv)=> mv.id === id);
-  const result = await client
-    .db("B42WD")
-    .collection("movies")
-    .updateOne({ id: id }, { $set: data });
-  response.send(result);
-});
+app.use("/movies", moviesRouter);
 
 app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
 
